ba6191085eb2a5789ae8d539e80d80f7e886f389
ElmaHuang <huanghuei0206@gmail.com>
2017-12-02 17:12:10 +0800

    ww

--- compute_node/InstanceFailures.py
+++ compute_node/InstanceFailures.py
@@ -116,7 +116,8 @@ class InstanceFailure(threading.Thread):
         instance = []
         #[['id:8f3340f3-0c48-4333-98e3-96f62df41f21', 'name:instance-00000346', 'host:compute3', 'status:ACTIVE', "network:{'selfservice':", "['192.168.1.8',", "'192.168.0.212']}
         instance = re.split(r'( \' |\s)\s*', string)
-        return instance
+        #return instance
+        print instance
 
 if __name__ == '__main__':
     a = InstanceFailure()
