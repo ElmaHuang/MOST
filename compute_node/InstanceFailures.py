ccfbaa8ac60dbe4b779aa9c97e746796278b09ec
ElmaHuang <huanghuei0206@gmail.com>
2017-12-02 17:24:46 +0800

    test

--- compute_node/InstanceFailures.py
+++ compute_node/InstanceFailures.py
@@ -115,12 +115,12 @@ class InstanceFailure(threading.Thread):
     def _splitString(self,string):
         instance = []
         #[['id:8f3340f3-0c48-4333-98e3-96f62df41f21', 'name:instance-00000346', 'host:compute3', 'status:ACTIVE', "network:{'selfservice':", "['192.168.1.8',", "'192.168.0.212']}
-        inst = re.split(r'( \' |\s)\s*', string)
+        inst = list(string)
         #return instance
         for str in inst:
-            if str.isalpha():
-                instance.append(str)
-        print instance
+            if not str.isalpha() or not str.isdigit():
+                inst.remove(str)
+        print inst
 
 if __name__ == '__main__':
     a = InstanceFailure()
