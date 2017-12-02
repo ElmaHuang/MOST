128c0c72c0923cf42cf563591cecac0ae931a358
ElmaHuang <huanghuei0206@gmail.com>
2017-12-02 18:37:12 +0800

    test

--- compute_node/InstanceFailures.py
+++ compute_node/InstanceFailures.py
@@ -119,9 +119,12 @@ class InstanceFailure(threading.Thread):
         #['id:8f3340f3-0c48-4333-98e3-96f62df41f21', ' name:instance-00000346', ' host:compute3', ' status:ACTIVE', ' network:selfservice:', ' 192.168.1.8', '', ' 192.168.0.212']
         inst = "".join(inst).split(",")
         for str in inst:
+            str = str.strip(" ")
             str = str.split(":")
-            if str !="":
-                instance.append(str)
+            for c in str :
+                if c ==" ":
+                    str.remove(c)
+            instance.append(str)
         print instance
 
 if __name__ == '__main__':
