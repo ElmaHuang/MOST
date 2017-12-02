80df9f859d39b2c3b0d092f27a8002fcbac0a95d
ElmaHuang <huanghuei0206@gmail.com>
2017-12-02 18:11:07 +0800

    test

--- compute_node/InstanceFailures.py
+++ compute_node/InstanceFailures.py
@@ -115,12 +115,13 @@ class InstanceFailure(threading.Thread):
     def _splitString(self,string):
         instance = []
         #[['id:8f3340f3-0c48-4333-98e3-96f62df41f21', 'name:instance-00000346', 'host:compute3', 'status:ACTIVE', "network:{'selfservice':", "['192.168.1.8',", "'192.168.0.212']}
-        inst = re.split('[\[\]\']', '', string)
+        inst = re.sub('[\[\]{}\'"]', '', string)
+        #['id:8f3340f3-0c48-4333-98e3-96f62df41f21', ' name:instance-00000346', ' host:compute3', ' status:ACTIVE', ' network:selfservice:', ' 192.168.1.8', '', ' 192.168.0.212']
         #return instance
         for str in inst:
-            print "ele:", str,"\n"
-            #str.strip(" ' ")
-        print inst
+            str = str.split(":")
+            instance.append(str)
+        print instance
 
 if __name__ == '__main__':
     a = InstanceFailure()
