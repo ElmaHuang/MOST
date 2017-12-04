ccdf5788e34f85127804c2f5e3967a891439b471
ElmaHuang <huanghuei0206@gmail.com>
2017-12-04 18:26:38 +0800

    fix recovery

--- compute_node/RecoveryInstance.py
+++ compute_node/RecoveryInstance.py
@@ -5,12 +5,20 @@ class RecoveryInstance():
         pass
 
     def rebootInstance(self,fail_instance):
+        # [
+        #  ['id', '8f3340f3-0c48-4333-98e3-96f62df41f21'],
+        #  ['name', 'instance-00000346'],
+        #  ['host', 'compute3'], ['status', 'ACTIVE'],
+        #  ['network', 'selfservice'],
+        #  ['192.168.1.8,'],
+        #  ['192.168.0.212']
+        # ]
         instance_name = ""
         # reboot vm
         for info in fail_instance:
-            if "name" in info:
-                instance_name = info.split(":")
-                instance_name = instance_name[-1]
+            if "name" in info[0]:
+                instance_name = info[1]
+                #instance_name = instance_name[-1]
         command = "virsh reset "+ instance_name
         response = subprocess.check_output(command, shell=True)
         if "reset" in response:
