7103c7d21e060a726e87835aecfea6bd599697be
ElmaHuang <huanghuei0206@gmail.com>
2017-12-18 13:58:45 +0800

    merge detector

--- IPMIModule.py
+++ IPMIModule.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+#########################################################
+#:Date: 2017/12/13
+#:Version: 1
+#:Authors:
+#    - Elma Huang <huanghuei0206@gmail.com>
+#    - LSC <sclee@g.ncu.edu.tw>
+#:Python_Version: 2.7
+#:Platform: Unix
+#:Description:
+#	This is a class maintains IPMI command operation
+##########################################################
 
 import subprocess
 import logging
