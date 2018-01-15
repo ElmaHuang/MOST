6161c06ef9297d888336330ac463ab78a718ac9a
ElmaHuang <huanghuei0206@gmail.com>
2018-01-15 20:49:35 +0800

    fix

--- UnitTest/tests/UT_Recover_VM_Crash.py
+++ UnitTest/tests/UT_Recover_VM_Crash.py
@@ -1,7 +1,7 @@
+import os
 import subprocess
 import sys
 import time
-import os
 
 import Postprocess
 import Preprocess
@@ -30,7 +30,6 @@ def run():
             return True
         else:
             return False
-
     except Exception as e:
         print str(e)
         return False
@@ -85,7 +84,8 @@ def detect_instance_status(timeout=5):
     result = False
     try:
         while timeout > 0:
-            if Preprocess._get_instance_status() == "ACTIVE":
+            status = Preprocess._get_instance_status()
+            if "ACTIVE" in status:
                 return True
             else:
                 timeout -= 1
@@ -98,11 +98,12 @@ def detect_instance_status(timeout=5):
 
 
 def _instance_failure(client, instance_name):
-    result = False
-    # i,o,e = _remote_exec(client,"killall python; cd /home/"+HOST+"/Desktop/MOST/HASS/compute_node;")
+    # result = False
     cmd = "ps aux | grep " + instance_name + " | awk '{ print $2 }'"
+    # cmd = "sudo hostname"
     i, o, e = _remote_exec(client, cmd)
-    pid = o
+    print o.read()
+    pid = o.read()
     print "pid:", pid
-    kill_cmd = "sudo kill -9 %s" % pid
-    _remote_exec(client, kill_cmd)
+    # kill_cmd = "sudo kill -9 %s" % pid
+    # _remote_exec(client, kill_cmd)
